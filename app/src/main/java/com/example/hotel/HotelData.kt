fc9fff6d2559c8aef1ae5804c8b7bf3d9d93682e
DulanWeerasekara <dulanrashmika929@gmail.com>
2023-04-24 18:47:47 +0530

    initial commit

---
+++ app/src/main/java/com/example/hotel/HotelData.kt
@@ -0,0 +1,4 @@
+package com.example.hotel
+
+data class HotelData(var hotelName : String?=null,var hotelAddress : String?=null,var hotelEmail : String?=null,var hotelPhone : String?=null,var hotelPrice : String?=null
+)
---
+++ app/src/main/java/com/example/hotel/HotelForm.kt
@@ -0,0 +1,70 @@
+package com.example.hotel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.hotel.databinding.FragmentHotelFormBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+
+
+class HotelForm : Fragment() {
+    lateinit var hotelName:String
+    lateinit var hotelAddress:String
+    lateinit var hotelEmail:String
+    lateinit var hotelPhone:String
+    lateinit var hotelPrice:String
+
+    lateinit var dataBase: DatabaseReference
+    private lateinit var binding: FragmentHotelFormBinding
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+
+
+
+
+    ): View? {
+        // Inflate the layout for this fragment
+        binding=FragmentHotelFormBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var myBundle=arguments
+        hotelName= myBundle?.getString("hotelName").toString()
+
+        binding.hotelRegButton.setOnClickListener{
+            hotelName=binding.hotelName.text.toString()
+            hotelAddress=binding.hotelAddress.text.toString()
+            hotelEmail=binding.hotelEmail.text.toString()
+            hotelPhone=binding.hotelPhone.text.toString()
+            hotelPrice=binding.hotelPrice.text.toString()
+
+            saveHotelInDatabase(hotelName,hotelAddress,hotelEmail,hotelPhone,hotelPrice)
+        }
+
+    }
+    private fun saveHotelInDatabase(hotelName:String,hotelAddress:String,hotelEmail:String,hotelPhone:String,hotelPrice:String){
+        val hotelData=HotelData(hotelName,hotelAddress,hotelEmail,hotelPhone,hotelPrice)
+        dataBase= FirebaseDatabase.getInstance().getReference("Hotel")
+        var childValue=hotelName
+        dataBase.child(childValue).setValue(hotelData).addOnSuccessListener{
+            Toast.makeText(activity, "complete", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
+
+
+}
