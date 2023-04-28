104502dc226a37ed23a4e73cfbfdfaca74d5550a
DulanWeerasekara <dulanrashmika929@gmail.com>
2023-04-28 14:49:21 +0530

    hotel list recycle view

--- app/src/main/java/com/example/hotel/MainActivity.kt
+++ app/src/main/java/com/example/hotel/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.hotel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button1 = findViewById<Button>(R.id.button1)
+
+        button1.setOnClickListener{
+            val intent1= Intent(this, hotelListPage::class.java)
+            startActivity(intent1)
+
+        }
+
+
+
+
+
+
     }
 }
