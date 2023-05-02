4630cbb868c7b3abbc544687ab565c4233c68a27
DulanWeerasekara <dulanrashmika929@gmail.com>
2023-05-02 21:51:42 +0530

    Book hotel

---
+++ app/src/main/java/com/example/hotel/hotelBookData.kt
@@ -0,0 +1,11 @@
+package com.example.hotel
+
+data class hotelBookData(
+    var hotelName : String?=null,
+    var hotelAddress : String?=null,
+    var hotelPhone : String?=null,
+    var hotelPrice : String?=null,
+    var CustomerName : String?=null,
+    var BookDays:String?=null,
+
+)
